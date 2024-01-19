ead0a1c8246f392468ddd3676a2eec0bbb23f769
skilljeminkikani <skilljeminkikani9510@gmail.com>
2024-01-19 10:43:31 +0530

    changes

--- app.js
+++ app.js
@@ -1,73 +1,71 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 
-var routes = require('./routes/index');
-var user = require('./routes/user');
-var startup = require('./routes/startup');
-var gameMenu = require('./routes/gameMenu'); 
-var gameplay = require('./routes/gameplay'); 
+var routes = require("./routes/index");
+var user = require("./routes/user");
+var startup = require("./routes/startup");
+var gameMenu = require("./routes/gameMenu");
+var gameplay = require("./routes/gameplay");
 
 var app = express();
 
-
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
-app.use('/startup', startup);
-app.use('/user', user);
-app.use('/gameMenu', gameMenu);
-app.use('/gameplay', gameplay);
+app.use("/", routes);
+app.use("/startup", startup);
+app.use("/user", user);
+app.use("/gameMenu", gameMenu);
+app.use("/gameplay", gameplay);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+if (app.get("env") === "development") {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err,
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {},
+  });
 });
 
-
 // var debug = require('debug')('code');
-var io = require('./lib/io');
+var io = require("./lib/io");
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get("port"), function () {
   console.log("http://localhost:" + server.address().port);
 });
 
